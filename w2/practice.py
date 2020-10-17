b234a8971343c765124ede21c3ca53e88764de12
Yuki Sakuma <kumasan.04.24.14@gmail.com>
2020-10-17 16:56:15 +0900

    [Add] first commit

---
+++ w2/practice.py
@@ -0,0 +1,35 @@
+print("hello")
+
+print(1+1)
+print(2-1)
+print(3*2)
+print(5/2)
+print(5//2)
+print(5%2)
+print(5**2)
+
+num_list = [2, 3, 5, 7]
+print(num_list)
+
+my_dict = {'name': "Yuki Sakuma"}
+print(my_dict['name'])
+
+num_set = {2, 3, 5, 7}
+print(num_set)
+
+num_taple = (2, 3, 5, 7)
+print(num_taple)
+
+set_ex = {2, 3, 2, 4, 3, 5, 6, 7, 2}
+print(set_ex)
+
+#配列の主な種類として list[], dict['':], set{}, taple()の４種類 listは基本的な配列 dictは基本的な配列だが、要素の番号を自分でつけれられる（要素のはんごうの代わりになるものをkeyと言う） setは配列だが、配列内に重複する値があれば、それは最初に存在するものが優先される。重複していたあとの要素は無視される。 tapleは最初に代入していこう変更されることはない。
+
+x = 0
+if x == 1:
+  print("hello")
+
+  x = [1, 2, 3, 4, 5, 6]
+for i in x:
+  if (i % 2):
+    print(i);
